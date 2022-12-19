99b033b5ea9533609961325a74269432f3087a02
herdal06 <h.erdal721@gmail.com>
2022-12-19 15:27:40 +0300

    Create MovieService and MoviePagingSource

--- app/src/main/java/com/herdal/moviehouse/utils/ApiConstants.kt
+++ app/src/main/java/com/herdal/moviehouse/utils/ApiConstants.kt
@@ -3,8 +3,13 @@ package com.herdal.moviehouse.utils
 object ApiConstants {
     const val language = "en-US"
     const val BASE_URL = "https://api.themoviedb.org/"
+    const val STARTING_PAGE = 1
 
     object Endpoints {
         const val GENRES = "3/genre/movie/list"
+        const val POPULAR = "3/movie/popular"
+        const val TOP_RATED = "3/movie/top_rated"
+        const val UPCOMING = "3/movie/upcoming"
+        const val NOW_PLAYING = "3/movie/latest"
     }
 }
