0462b1d7c5565665f7edfdb4edeee127a53329d7
herdal06 <h.erdal721@gmail.com>
2022-12-19 13:46:54 +0300

    Add base url

--- app/src/main/java/com/herdal/moviehouse/utils/ApiConstants.kt
+++ app/src/main/java/com/herdal/moviehouse/utils/ApiConstants.kt
@@ -2,7 +2,7 @@ package com.herdal.moviehouse.utils
 
 object ApiConstants {
     const val language = "en-US"
-    const val BASE_URL = ""
+    const val BASE_URL = "https://api.themoviedb.org/"
 
     object Endpoints {
         const val GENRES = "3/genre/movie/list"
