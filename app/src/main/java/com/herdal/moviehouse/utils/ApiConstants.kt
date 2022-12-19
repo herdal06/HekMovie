ccf8ce0be828bf6301f61297ebe195a71c90a757
herdal06 <h.erdal721@gmail.com>
2022-12-19 12:16:16 +0300

    Provide retrofit and GenreService

--- app/src/main/java/com/herdal/moviehouse/utils/ApiConstants.kt
+++ app/src/main/java/com/herdal/moviehouse/utils/ApiConstants.kt
@@ -2,6 +2,8 @@ package com.herdal.moviehouse.utils
 
 object ApiConstants {
     const val language = "en-US"
+    const val BASE_URL = ""
+
     object Endpoints {
         const val GENRES = "3/genre/movie/list"
     }
