965f921dd617e338fa96dd4c6a7c93a3280054f7
herdal06 <h.erdal721@gmail.com>
2022-12-19 23:15:28 +0300

    Set on click listener to every movies

--- app/src/main/java/com/herdal/moviehouse/ui/home/adapter/movie/MovieViewHolder.kt
+++ app/src/main/java/com/herdal/moviehouse/ui/home/adapter/movie/MovieViewHolder.kt
@@ -6,11 +6,16 @@ import com.herdal.moviehouse.domain.uimodel.MovieUiModel
 import com.herdal.moviehouse.utils.extensions.executeWithAction
 
 class MovieViewHolder(
-    private val binding: ItemMovieBinding
+    private val binding: ItemMovieBinding,
+    private val onClickMovie: ((movieId: Int) -> Unit)?
 ) : RecyclerView.ViewHolder(binding.root) {
     fun bind(movie: MovieUiModel) = binding.apply {
         binding.executeWithAction {
             this.movie = movie
+        }
+
+        itemView.setOnClickListener {
+            onClickMovie?.invoke(movie.id)
         }
     }
 }
