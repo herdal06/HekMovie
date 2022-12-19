4752048ea02f0936d56f24d8583d029eb32ec648
herdal06 <h.erdal721@gmail.com>
2022-12-19 13:47:03 +0300

    Create HomeFragment and view extension

---
+++ app/src/main/java/com/herdal/moviehouse/ui/home/HomeFragment.kt
@@ -0,0 +1,76 @@
+package com.herdal.moviehouse.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.herdal.moviehouse.R
+import com.herdal.moviehouse.common.Resource
+import com.herdal.moviehouse.databinding.FragmentHomeBinding
+import com.herdal.moviehouse.ui.home.adapter.genre.GenreAdapter
+import com.herdal.moviehouse.utils.extensions.hide
+import com.herdal.moviehouse.utils.extensions.show
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+    private val viewModel: HomeViewModel by viewModels()
+
+    private val genreAdapter: GenreAdapter by lazy {
+        GenreAdapter()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        setupRecyclerViews()
+        collectApiRequest()
+        return binding.root
+    }
+
+    private fun setupRecyclerViews() = binding.apply {
+        rvGenres.adapter = genreAdapter
+    }
+
+    private fun collectApiRequest() = binding.apply {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    // genres
+                    viewModel.getAllGenres()
+                    viewModel.genres.collect { res ->
+                        when (res) {
+                            is Resource.Loading -> {
+                                tvGenreErrorMessage.hide()
+                                rvGenres.hide()
+                            }
+                            is Resource.Success -> {
+                                tvGenreErrorMessage.hide()
+                                pbGenres.hide()
+                                rvGenres.show()
+                                genreAdapter.submitList(res.data)
+                            }
+                            is Resource.Error -> {
+                                tvGenreErrorMessage.show()
+                                pbGenres.hide()
+                                rvGenres.hide()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
