db7ea1bea47112dc2db26313e2cc88f90f636ce0
herdal06 <h.erdal721@gmail.com>
2022-12-19 12:29:25 +0300

    Create GetGenresUseCase

---
+++ app/src/main/java/com/herdal/moviehouse/domain/use_case/genre/GetGenresUseCase.kt
@@ -0,0 +1,28 @@
+package com.herdal.moviehouse.domain.use_case.genre
+
+import com.herdal.moviehouse.common.Resource
+import com.herdal.moviehouse.domain.repository.GenreRepository
+import com.herdal.moviehouse.domain.uimodel.GenreUiModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import okio.IOException
+import retrofit2.HttpException
+import timber.log.Timber
+import javax.inject.Inject
+
+class GetGenresUseCase @Inject constructor(
+    private val genreRepository: GenreRepository
+) {
+    operator fun invoke(): Flow<Resource<List<GenreUiModel>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val genres = genreRepository.getGenres()
+            Timber.d("$genres")
+            emit(Resource.Success(data = genres))
+        } catch (e: IOException) {
+            emit(Resource.Error(message = e.message))
+        } catch (e: HttpException) {
+            emit(Resource.Error(message = e.message))
+        }
+    }
+}
