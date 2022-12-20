b46c3448c2306ad467f342cb7175dc71104b4205
herdal06 <h.erdal721@gmail.com>
2022-12-20 17:42:46 +0300

    Get recommended movies on tab layout

---
+++ app/src/main/java/com/herdal/moviehouse/ui/recommended_movies/RecommendedMoviesFragment.kt
@@ -0,0 +1,68 @@
+package com.herdal.moviehouse.ui.recommended_movies
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.herdal.moviehouse.databinding.FragmentRecommendedMoviesBinding
+import com.herdal.moviehouse.ui.home.adapter.movie.MovieAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import timber.log.Timber
+
+@AndroidEntryPoint
+class RecommendedMoviesFragment(
+    private val movieId: Int
+) : Fragment() {
+
+    private var _binding: FragmentRecommendedMoviesBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    private val viewModel: RecommendedMoviesViewModel by viewModels()
+
+    private val recommendedMoviesAdapter: MovieAdapter by lazy {
+        MovieAdapter(::onClickMovie)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRecommendedMoviesBinding.inflate(inflater, container, false)
+        val view = binding.root
+        setupRv()
+        observeSimilarMovies(movieId)
+        return view
+    }
+
+    private fun setupRv() = binding.apply {
+        rvRecommended.adapter = recommendedMoviesAdapter
+    }
+
+    private fun observeSimilarMovies(movieId: Int) = lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            viewModel.getRecommendedMovies(movieId).observe(viewLifecycleOwner) {
+                recommendedMoviesAdapter.submitData(lifecycle, it)
+                Timber.d("$it")
+            }
+        }
+    }
+
+    private fun onClickMovie(movieId: Int) {
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
