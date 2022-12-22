1e9285aa3d8154265fbf88619bde84c3a6726b40
herdal06 <h.erdal721@gmail.com>
2022-12-22 14:03:36 +0300

    Create PersonAdapter

---
+++ app/src/main/java/com/herdal/moviehouse/ui/home/adapter/people/PersonViewHolder.kt
@@ -0,0 +1,21 @@
+package com.herdal.moviehouse.ui.home.adapter.people
+
+import androidx.recyclerview.widget.RecyclerView
+import com.herdal.moviehouse.databinding.ItemPersonBinding
+import com.herdal.moviehouse.domain.uimodel.person.PersonUiModel
+import com.herdal.moviehouse.utils.extensions.executeWithAction
+
+class PersonViewHolder(
+    private val binding: ItemPersonBinding,
+    private val onClickPerson: ((personId: Int) -> Unit)?
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(person: PersonUiModel) = binding.apply {
+        binding.executeWithAction {
+            this.person = person
+        }
+
+        itemView.setOnClickListener {
+            onClickPerson?.invoke(person.id)
+        }
+    }
+}
