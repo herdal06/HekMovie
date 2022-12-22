d572db0a966bfc2893e6bd1f1de2cdb09284322a
herdal06 <h.erdal721@gmail.com>
2022-12-22 15:27:45 +0300

    Observe people and display on the home screen

--- app/src/main/java/com/herdal/moviehouse/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/herdal/moviehouse/ui/home/HomeViewModel.kt
@@ -6,11 +6,13 @@ import androidx.paging.cachedIn
 import com.herdal.moviehouse.common.Resource
 import com.herdal.moviehouse.domain.uimodel.GenreUiModel
 import com.herdal.moviehouse.domain.uimodel.MovieUiModel
+import com.herdal.moviehouse.domain.uimodel.person.PersonUiModel
 import com.herdal.moviehouse.domain.use_case.genre.GetGenresUseCase
 import com.herdal.moviehouse.domain.use_case.movie.GetNowPlayingMoviesUseCase
 import com.herdal.moviehouse.domain.use_case.movie.GetPopularMoviesUseCase
 import com.herdal.moviehouse.domain.use_case.movie.GetTopRatedMoviesUseCase
 import com.herdal.moviehouse.domain.use_case.movie.GetUpcomingMoviesUseCase
+import com.herdal.moviehouse.domain.use_case.person.GetPopularPeopleUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -25,7 +27,8 @@ class HomeViewModel @Inject constructor(
     private val getPopularMoviesUseCase: GetPopularMoviesUseCase,
     private val getTopRatedMoviesUseCase: GetTopRatedMoviesUseCase,
     private val getUpcomingMoviesUseCase: GetUpcomingMoviesUseCase,
-    private val getNowPlayingMoviesUseCase: GetNowPlayingMoviesUseCase
+    private val getNowPlayingMoviesUseCase: GetNowPlayingMoviesUseCase,
+    private val getPopularPeopleUseCase: GetPopularPeopleUseCase
 ) : ViewModel() {
     private val _genres =
         MutableStateFlow<Resource<List<GenreUiModel>>>(Resource.Loading())
@@ -42,6 +45,9 @@ class HomeViewModel @Inject constructor(
 
     private val _nowPlayingMovies =
         MutableLiveData<PagingData<MovieUiModel>>()
+
+    private val _popularPeople =
+        MutableLiveData<PagingData<PersonUiModel>>()
 
     fun getAllGenres() {
         getGenresUseCase.invoke()
@@ -86,6 +92,13 @@ class HomeViewModel @Inject constructor(
     fun getNowPlayingMovies(): LiveData<PagingData<MovieUiModel>> {
         val response = getNowPlayingMoviesUseCase.invoke().cachedIn(viewModelScope).asLiveData()
         _nowPlayingMovies.value = response.value
+        Timber.d("$response")
+        return response
+    }
+
+    fun getPopularPeople(): LiveData<PagingData<PersonUiModel>> {
+        val response = getPopularPeopleUseCase.invoke().cachedIn(viewModelScope).asLiveData()
+        _popularPeople.value = response.value
         Timber.d("$response")
         return response
     }
