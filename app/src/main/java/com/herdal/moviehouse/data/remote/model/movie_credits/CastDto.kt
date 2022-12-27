1264dc552a43c6904547899d3afb730a5aa06138
herdal06 <h.erdal721@gmail.com>
2022-12-27 11:46:18 +0300

    Add JsonClass and Json annotations to movie credits dtos

--- app/src/main/java/com/herdal/moviehouse/data/remote/model/movie_credits/CastDto.kt
+++ app/src/main/java/com/herdal/moviehouse/data/remote/model/movie_credits/CastDto.kt
@@ -1,16 +1,32 @@
 package com.herdal.moviehouse.data.remote.model.movie_credits
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class CastDto(
+    @Json(name ="adult")
     val adult: Boolean,
+    @Json(name ="cast_id")
     val cast_id: Int,
+    @Json(name ="character")
     val character: String,
+    @Json(name ="credit_id")
     val credit_id: String,
+    @Json(name ="gender")
     val gender: Int,
+    @Json(name ="id")
     val id: Int,
+    @Json(name ="known_for_department")
     val known_for_department: String,
+    @Json(name ="name")
     val name: String,
+    @Json(name ="order")
     val order: Int,
+    @Json(name ="original_name")
     val original_name: String,
+    @Json(name ="popularity")
     val popularity: Double,
+    @Json(name ="profile_path")
     val profile_path: String
 )
--- app/src/main/java/com/herdal/moviehouse/data/remote/model/movie_credits/MovieCreditsResponse.kt
+++ app/src/main/java/com/herdal/moviehouse/data/remote/model/movie_credits/MovieCreditsResponse.kt
@@ -1,7 +1,14 @@
 package com.herdal.moviehouse.data.remote.model.movie_credits
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class MovieCreditsResponse(
+    @Json(name ="cast")
     val cast: List<CastDto>,
+    @Json(name ="crew")
     val crew: List<CrewDto>,
+    @Json(name ="id")
     val id: Int
 )
