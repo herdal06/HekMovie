865ec72e7011400bbbb19a045c8d63373d93fb6a
herdal06 <h.erdal721@gmail.com>
2022-12-27 16:53:41 +0300

    Get movies by genre

---
+++ app/src/main/java/com/herdal/moviehouse/ui/movies_by_genre/MoviesByGenreFragment.kt
@@ -0,0 +1,82 @@
+package com.herdal.moviehouse.ui.movies_by_genre
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.paging.map
+import com.herdal.moviehouse.databinding.FragmentMoviesByGenreBinding
+import com.herdal.moviehouse.domain.uimodel.GenreUiModel
+import com.herdal.moviehouse.ui.home.adapter.movie.MovieAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import timber.log.Timber
+
+@AndroidEntryPoint
+class MoviesByGenreFragment : Fragment() {
+
+    private var _binding: FragmentMoviesByGenreBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    private val viewModel: MoviesByGenreViewModel by viewModels()
+
+    private val moviesByGenreAdapter: MovieAdapter by lazy {
+        MovieAdapter(::onClickMovie)
+    }
+
+    private val navigationArgs: MoviesByGenreFragmentArgs by navArgs()
+
+    private fun getArgs() = navigationArgs.genre
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMoviesByGenreBinding.inflate(inflater, container, false)
+        val view = binding.root
+        setupRecyclerView()
+        observeMovies(getArgs())
+        return view
+    }
+
+    private fun setupRecyclerView() = binding.apply {
+        rvMoviesByGenre.adapter = moviesByGenreAdapter
+    }
+
+    private fun observeMovies(genre: GenreUiModel) = lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            viewModel.getMoviesByGenre(genre.id).observe(viewLifecycleOwner) {
+                moviesByGenreAdapter.submitData(lifecycle, it)
+                it.map {
+                    (activity as AppCompatActivity).supportActionBar?.title = genre.name
+                }
+                Timber.d("$it")
+            }
+        }
+    }
+
+    private fun onClickMovie(movieId: Int) {
+        val action =
+            MoviesByGenreFragmentDirections.actionMoviesByGenreFragmentToMovieDetailsFragment(
+                movieId
+            )
+        findNavController().navigate(action)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
