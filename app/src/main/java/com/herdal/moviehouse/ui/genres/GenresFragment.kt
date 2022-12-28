6cec16346b21feb1e650d11bbb370284d95525c5
herdal06 <h.erdal721@gmail.com>
2022-12-28 11:21:48 +0300

    Collect genres and display on the screen

---
+++ app/src/main/java/com/herdal/moviehouse/ui/genres/GenresFragment.kt
@@ -0,0 +1,91 @@
+package com.herdal.moviehouse.ui.genres
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import com.herdal.moviehouse.common.Resource
+import com.herdal.moviehouse.databinding.FragmentGenresBinding
+import com.herdal.moviehouse.domain.uimodel.GenreUiModel
+import com.herdal.moviehouse.ui.home.adapter.genre.GenreAdapter
+import com.herdal.moviehouse.utils.extensions.hide
+import com.herdal.moviehouse.utils.extensions.show
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class GenresFragment : Fragment() {
+
+    private var _binding: FragmentGenresBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    private val genreAdapter: GenreAdapter by lazy {
+        GenreAdapter(::onClickGenre)
+    }
+
+    private val viewModel: GenresViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentGenresBinding.inflate(inflater, container, false)
+        val view = binding.root
+        setupRecyclerView()
+        collectGenres()
+        return view
+    }
+
+    private fun collectGenres() = binding.apply {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel.getAllGenres()
+                    viewModel.genres.collect { res ->
+                        when (res) {
+                            is Resource.Loading -> {
+                                tvGenresErrorMessage.hide()
+                                rvAllGenre.hide()
+                            }
+                            is Resource.Success -> {
+                                tvGenresErrorMessage.hide()
+                                pbGenre.hide()
+                                rvAllGenre.show()
+                                genreAdapter.submitList(res.data)
+                            }
+                            is Resource.Error -> {
+                                tvGenresErrorMessage.show()
+                                pbGenre.hide()
+                                rvAllGenre.hide()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setupRecyclerView() = binding.apply {
+        rvAllGenre.adapter = genreAdapter
+    }
+
+    private fun onClickGenre(genre: GenreUiModel) {
+        val action = GenresFragmentDirections.actionGenresFragmentToMoviesByGenreFragment(genre)
+        findNavController().navigate(action)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/herdal/moviehouse/ui/genres/GenresViewModel.kt
@@ -0,0 +1,42 @@
+package com.herdal.moviehouse.ui.genres
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.herdal.moviehouse.common.Resource
+import com.herdal.moviehouse.domain.uimodel.GenreUiModel
+import com.herdal.moviehouse.domain.use_case.genre.GetGenresUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@HiltViewModel
+class GenresViewModel @Inject constructor(
+    private val getGenresUseCase: GetGenresUseCase
+) : ViewModel() {
+
+    private val _genres =
+        MutableStateFlow<Resource<List<GenreUiModel>>>(Resource.Loading())
+    val genres: StateFlow<Resource<List<GenreUiModel>>> = _genres
+
+    fun getAllGenres() {
+        getGenresUseCase.invoke()
+            .onEach { resource ->
+                when (resource) {
+                    is Resource.Loading -> {
+                        _genres.value = Resource.Loading()
+                    }
+                    is Resource.Success -> {
+                        if (resource.data != null) {
+                            _genres.value = Resource.Success(resource.data)
+                        }
+                    }
+                    is Resource.Error -> {
+                        _genres.value = Resource.Error(resource.message)
+                    }
+                }
+            }.launchIn(viewModelScope)
+    }
+}
